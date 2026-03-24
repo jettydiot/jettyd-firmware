e22a9c8d3ecc3c985bc95b34c1ab6a749075dc84
Tom <tomzo@clawfed.com>
2026-03-24 18:05:16 +0200

    fix: ina219 - add legacy driver/i2c.h and freertos includes

--- drivers/ina219/ina219.c
+++ drivers/ina219/ina219.c
@@ -8,8 +8,10 @@
 
 #include "ina219.h"
 #include "jettyd_driver.h"
-#include "driver/i2c_master.h"
+#include "driver/i2c.h"
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include <string.h>
 
 static const char *TAG = "drv_ina219";
